960894f8f49887d098c8e6362570899dfb1fb0a6
NidhiK-104 <73216471+NidhiK-104@users.noreply.github.com>
2021-12-15 22:03:27 +0530

    Create 11_HouseRotation.cpp

---
+++ 11_HouseRotation.cpp
@@ -0,0 +1,98 @@
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+
+float house[11][2] = {{100, 200}, {200, 250}, {300, 200}, {100, 200}, {100, 100}, {175, 100},{175, 150}, {225, 150}, {225, 100}, {300, 100}, {300, 200}};
+int angle;
+float m, c, theta;
+
+void myInit()
+{
+	glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-450, 450, -450, 450);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void display()
+{
+	myInit();
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < 11; i++)
+		glVertex2fv(house[i]);
+	glEnd();
+	glFlush();
+	glPushMatrix(); //ROTATED HOUSE
+	glTranslatef(100, 100, 0);
+	glRotatef(angle, 0, 0, 1);
+	glTranslatef(-100, -100, 0);
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < 11; i++)
+		glVertex2fv(house[i]);
+	glEnd();
+	glPopMatrix();
+	glFlush();
+}
+
+void display2()
+{
+	myInit();
+	glColor3f(1, 0, 0);//Normal house
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < 11; i++)
+		glVertex2fv(house[i]);
+	glEnd();
+	glFlush();
+	float x1 = 0, x2 = 500; //Line
+	float y1 = m * x1 + c;
+	float y2 = m * x2 + c;
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+		glVertex2f(x1, y1);
+		glVertex2f(x2, y2);
+	glEnd();
+	glFlush();
+	glPushMatrix();//Reflected
+	glTranslatef(0, c, 0);
+	theta = atan(m);
+	theta = theta * 180 / 3.14;		//radians to degrees
+	glRotatef(theta, 0, 0, 1);
+	glScalef(1, -1, 1);
+	glRotatef(-theta, 0, 0, 1);
+	glTranslatef(0, -c, 0);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < 11; i++)
+		glVertex2fv(house[i]);
+	glEnd();
+	glPopMatrix();
+	glFlush();
+}
+
+void mouse(int btn, int state, int x, int y) 
+{
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) 
+		display();
+	else if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) 
+		display2();
+}
+
+void main(int argc, char **argv)
+{
+	printf("Enter the rotation angle:\n");
+	scanf_s("%d", &angle);
+	printf("Enter c and m for the line y = mx + c:\n");
+	scanf_s("%f %f", &c, &m);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("House Rotation");
+	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
+	glutMainLoop();
+}
